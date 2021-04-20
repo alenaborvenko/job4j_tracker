12bc98ef8dd92f22ef8e8c76fd7a680d3fcf384b
Borvenko Alena <alenaborvenko@gmail.com>
2021-04-20 23:00:04 +0700

    7. Шаблон проектирования - Стратегия [#515378]

---
+++ src/main/java/ru/job4j/tracker/ReplaceAction.java
@@ -0,0 +1,22 @@
+package ru.job4j.tracker;
+
+public class ReplaceAction implements UserAction {
+    @Override
+    public String name() {
+        return "Edit item";
+    }
+
+    @Override
+    public boolean execute(Input input, Tracker tracker) {
+        System.out.println("=== Edit item ====");
+        int id = input.askInt("Enter id: ");
+        String name = input.askStr("Enter name: ");
+        Item item = new Item(name);
+        if (tracker.replace(id, item)) {
+            System.out.println("Edit was success.");
+        } else {
+            System.out.println("Edit wasn't success. No item with " + id + " id.");
+        }
+        return true;
+    }
+}
