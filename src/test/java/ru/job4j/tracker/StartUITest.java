98bab68c84ebc3311ad88a6dd51ae712d485856a
Borvenko Alena <alenaborvenko@gmail.com>
2021-04-20 21:50:06 +0700

    5.1. Тестирование. Подготовка данных. [#515366]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -15,4 +15,17 @@ public class StartUITest {
         Item expected = new Item("Fix PC");
         assertThat(created.getName(), is(expected.getName()));
     }
+
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new Item");
+        tracker.add(item);
+        String[] answers = new String[]{
+                String.valueOf(item.getId()),
+                "replaced item"};
+        StartUI.replaceItem(new StubInput(answers), tracker);
+        Item replaced = tracker.findById(item.getId());
+        assertThat(replaced.getName(), is("replaced item"));
+    }
 }
