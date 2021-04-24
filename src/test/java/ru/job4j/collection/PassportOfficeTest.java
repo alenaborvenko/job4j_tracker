ae2ffe67131760c48836eb4937d013364c70a522
Borvenko Alena <alenaborvenko@gmail.com>
2021-04-24 17:23:01 +0700

    1. Паспорт и жители [#515345]

---
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class PassportOfficeTest {
+
+    @Test
+    public void whenAddFewNewCitizenThenSuccessAdd() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen2 = new Citizen("4443f", "Alena Borvenko");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        office.add(citizen2);
+        List<Citizen> expected = new ArrayList<>(Arrays.asList(citizen, citizen2));
+        List<Citizen> rsl = new ArrayList<>();
+        rsl.add(office.get(citizen.getPassport()));
+        rsl.add(office.get(citizen2.getPassport()));
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenAddNotNewCitizenThenNotSuccessAdd() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        boolean add = office.add(citizen);
+        assertFalse(add);
+    }
+
+    @Test
+    public void whenGetNotFoundPassportThenNullValue() {
+        PassportOffice office = new PassportOffice();
+        Citizen actual = office.get("jhbj");
+        assertThat(actual, is(nullValue()));
+    }
+}
