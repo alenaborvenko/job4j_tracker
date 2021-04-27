9ef0afe744ecece9722dde2f9faefe88d32bd3d7
Borvenko Alena <alenaborvenko@gmail.com>
2021-04-27 21:55:24 +0700

    0. Stream API [#515167]

--- src/main/java/ru/job4j/stream/PositiveNums.java
+++ src/main/java/ru/job4j/stream/PositiveNums.java
@@ -8,14 +8,16 @@ import java.util.stream.Collectors;
 public class PositiveNums {
     public static void main(String[] args) {
         List<Integer> nums = new ArrayList<>(Arrays.asList(
-                1, -10, 0, 15, 90, -32, 8, -4, 2
+                1, -10, 0, 15, 60, -32, 8, -4, 2
         ));
         System.out.println("Первоначальный массив:");
         for (Integer num : nums) {
             System.out.print(num + " ");
         }
         System.out.println();
-        List<Integer> afterFilter = nums.stream().filter(i -> i > 0).collect(Collectors.toList());
+        List<Integer> afterFilter = nums.stream()
+                .filter(i -> i > 0)
+                .collect(Collectors.toList());
         System.out.println("После фильтрации:");
         for (Integer num : afterFilter) {
             System.out.print(num + " ");
