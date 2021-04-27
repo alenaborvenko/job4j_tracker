7f220425a5bd8b5fdbe1ff53ee02b473d2e790bf
Borvenko Alena <alenaborvenko@gmail.com>
2021-04-27 15:37:48 +0700

    2. Отсортировать департаменты [#515326]

---
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -0,0 +1,17 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class DepDescComp implements Comparator<String> {
+
+    @Override
+    public int compare(String o1, String o2) {
+        String[] dep1 = o1.split("/");
+        String[] dep2 = o2.split("/");
+        int rsl = dep2[0].compareTo(dep1[0]);
+        if (rsl != 0) {
+            return rsl;
+        }
+        return o1.compareTo(o2);
+    }
+}
